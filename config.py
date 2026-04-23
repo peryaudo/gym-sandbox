253f501f9f13732aac3f233bd34f91ec7f9e5c54
Tetsui Ohkubo <peryaudo@gmail.com>
2026-04-23 17:03:03 +0900

    Add LunarLander option

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ class Config:
     obs_dim: int = 4
     action_dim: int = 2
     n_episodes: int = 500
+
+# @dataclass
+# class Config:
+#     env_name: str = "LunarLander-v3"
+#     obs_dim: int = 8
+#     action_dim: int = 4
+#     n_episodes: int = 5000
